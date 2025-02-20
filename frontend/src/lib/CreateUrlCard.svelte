a6cd71a00c0be07745bfdedd097277ae08ce72da
Cooper Weiser <cooper@Coopers-MacBook-Pro.local>
2025-02-19 17:19:32 -0800

    Start frontend

--- frontend/src/lib/CreateUrlCard.svelte
+++
@@ -1,9 +0,0 @@
-<div class="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto my-auto h-full">
-
-  <h2 class="text-xl font-semibold text-gray-800 mb-4">Shorten Your URL</h2>
-
-  <input type="text" placeholder="Long URL" class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-
-  <button class="w-full mt-4 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition">Shorten</button>
-</div>
-
